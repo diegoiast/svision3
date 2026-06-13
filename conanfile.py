bcb59dce7bbfc6e95d6872f0150f033da9d77b9a
Diego Iastrubni <diegoiast@gmail.com>
2026-06-13 22:51:40 +0300

    conanfile.py: we don't need cairo
    If we remove Cairo from conan - it will come from the OS, as the
    CMakeFile will just assume it exists.

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,11 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 
-
 class ToolkitRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
-    options = {"with_cairo": [True, False]}
-    default_options = {"with_cairo": False}
 
     def requirements(self):
-        if self.settings.os not in ["Macos", "Windows"] or self.options.with_cairo:
-            self.requires("cairo/1.18.0")
         self.requires("spdlog/1.14.1")
         self.requires("catch2/3.7.1")
         self.requires("tomlplusplus/3.4.0")
