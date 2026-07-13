b2d3a70c3e8cdf2d9cc8af0010e6826068857d00
Diego Iastrubni <diegoiast@gmail.com>
2026-07-14 00:47:07 +0300

    Button: fix background painting
    Without this - all the button is drawed the same same background color,
    with this: only the button face has the background color.

--- include/toolkit/button.hpp
+++ include/toolkit/button.hpp
@@ -21,6 +21,7 @@ class Button : public Widget, public Fluent<Button> {
     nlohmann::json to_json() const override;
     void from_json(nlohmann::json const &j) override;
 
+    void paint_background(Painter &) override {}
     void paint(Painter &painter) override;
     bool handle_mouse(MouseEvent const &event) override;
     bool handle_key(KeyEvent const &event) override;
